d5f731ba3fa610b34cb97de2a5deb594b9e67364
Molinari <matteomolinari93@gmail.com>
2020-10-20 08:42:28 +0200

    Updated maven dependencies

--- src/main/java/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapper.java
+++ src/main/java/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapper.java
@@ -3,10 +3,12 @@ package guru.springframework.sfgrestdocsexample.web.mappers;
 import guru.springframework.sfgrestdocsexample.domain.Beer;
 import guru.springframework.sfgrestdocsexample.web.model.BeerDto;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by jt on 2019-05-25.
  */
+@Component
 @Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
 
